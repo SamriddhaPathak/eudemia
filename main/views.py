62c134e691ef4c9da18127d7777f3c1d4e1f61b9
Rasum <rasum.subedi13@gmail.com>
2025-01-02 17:32:56 +0545

    Create login page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import LoginForm
 
 # Create your views here.
 def index(request):
     return render(request, "main/index.html")
 
-def login(request):
-    return render(request, "main/login.html")
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/index.html')
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'main/login.html', context)
