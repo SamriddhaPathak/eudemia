127f8bd3755720da04e498907b6a0b222f01f5bd
Rasum <rasum.subedi13@gmail.com>
2025-01-04 15:24:11 +0545

    Make login form responsive

--- main/forms.py
+++ main/forms.py
@@ -5,14 +5,14 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=150,
         widget=forms.TextInput(attrs={
-            'class': 'form-control',
+            'class': 'form-control form-input',
             'placeholder': 'Username',
         }),
         label=''
     )
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'form-control',
+            'class': 'form-control form-input',
             'placeholder': 'Password',
         }),
         label=''
