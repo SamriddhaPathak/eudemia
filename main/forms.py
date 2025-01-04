d28328ec2ae57dc4a6a5b3c7f2375b6dd40338d5
Rasum <rasum.subedi13@gmail.com>
2025-01-04 23:29:59 +0545

    Add usertype selection to login form

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,16 @@ from django import forms
 from django.contrib import messages
 
 class LoginForm(forms.Form):
+    usertype = forms.ChoiceField(
+        widget = forms.RadioSelect(attrs={
+            'class': 'form-control form-select',
+        }),
+        choices = [
+            ('1', 'Teacher'),
+            ('2', 'Student'),
+            ('3', 'Parent'),
+        ]
+    )
     username = forms.CharField(
         max_length=150,
         widget=forms.TextInput(attrs={
