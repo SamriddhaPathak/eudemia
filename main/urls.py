0d29dd915dbf2457e8430c0080dc74bb6e4b8383
Rasum <rasum.subedi13@gmail.com>
2025-01-08 16:07:34 +0545

    change logo

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("users/", include("django.contrib.auth.urls")),
-    path("dashboard", views.dashboard_view, name="dashboard"),
+    path("dashboard/", views.dashboard_view, name="dashboard"),
 ]
   
