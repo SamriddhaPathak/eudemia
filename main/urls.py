87379ef065d35b3af1843ac0320230c72bd71df9
Samriddha Pathak <samriddhapathak123333@gmail.com>
2025-01-08 14:50:19 +0545

    minor changes

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("users/", include("django.contrib.auth.urls")),
+    path("users/", include("users.urls"))
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .forms import LoginForm
 
 # Create your views here.
@@ -11,7 +13,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('/index.html')
+                return redirect('')
             else:
                 messages.error(request, 'Invalid username or password.')
     else:
@@ -21,3 +23,4 @@ def login_view(request):
         'form': form,
     }
     return render(request, 'users/login.html', context)
+
