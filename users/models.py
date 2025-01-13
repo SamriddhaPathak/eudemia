e7b01a02ce151e37adbe4ae779209d0f199c141b
Rasum <rasum.subedi13@gmail.com>
2025-01-13 23:38:58 +0545

    change user_id foreign key fields into user

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Class(models.Model):
-    grade  = models.IntegerField(primary_key= True)
+    grade  = models.IntegerField(primary_key=True)
     name = models.CharField(max_length= 10)
     score = models.IntegerField()
 
@@ -12,29 +12,29 @@ class Class(models.Model):
 class Student(models.Model):
     dob = models.DateField()
     points = models.IntegerField()
-    grade = models.ForeignKey(Class, on_delete = models.CASCADE)
+    grade = models.ForeignKey(Class, on_delete=models.CASCADE)
     height = models.FloatField()
     weight = models.IntegerField()
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         # Use the related User's first and last name
-        return f"{self.user_id.first_name}_{self.user_id.last_name}"
+        return f"{self.user.first_name}_{self.user.last_name}"
 
 
 class Teacher(models.Model):
     ph_number = models.CharField(max_length=20)
-    grade = models.IntegerField()
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    grade = models.ForeignKey(Class, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user_id.first_name}_{self.user_id.last_name}"
+        return f"{self.user.first_name}_{self.user.last_name}"
 
 
 class Parent(models.Model):
     ph_number = models.CharField(max_length=20)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     students = models.ManyToManyField(Student)  
 
     def __str__(self):
-        return f"{self.user_id.first_name}_{self.user_id.last_name}"
+        return f"{self.user.first_name}_{self.user.last_name}"
