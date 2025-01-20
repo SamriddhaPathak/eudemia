0f8797ad85a04b879625abbac53fd8354b7330e7
Rasum <rasum.subedi13@gmail.com>
2025-01-20 13:32:49 +0545

    fix: inconsistent shadows in teacher and parents pages

--- main/static/main/js/script.js
+++ main/static/main/js/script.js
@@ -2,14 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     students = document.querySelectorAll(".child-name");
     students.forEach(element => {
         const initial_display = element.nextElementSibling.style.display;
+        const initial_display_next = element.nextElementSibling.nextElementSibling.style.display;
         element.nextElementSibling.style.display = "none";
+        element.nextElementSibling.nextElementSibling.style.setProperty("display", "none", "important");
         element.addEventListener("click", function (e) {
             const content = e.target.nextElementSibling;
+            const content_next = e.target.nextElementSibling.nextElementSibling;
             if (content.style.display === "none") {
                 content.style.display = initial_display;
+                content_next.style.display = initial_display_next
             }
             else {
                 content.style.display = "none";
+                content_next.style.setProperty("display", "none", "important");
             }
         });
 
