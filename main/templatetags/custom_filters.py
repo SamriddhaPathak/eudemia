1756c689eb1f810ccbe723866ba64ac9c2736993
Rasum <rasum.subedi13@gmail.com>
2025-01-23 02:17:42 +0545

    feat: display height in feet and inches

--- main/templatetags/custom_filters.py
+++ main/templatetags/custom_filters.py
@@ -1,4 +1,6 @@
 from django import template
+import math
+
 register = template.Library()
 
 @register.filter
@@ -15,3 +17,15 @@ def div(value, arg):
 @register.filter
 def mult(value, arg):
     return float(value) * float(arg)
+
+@register.filter
+def floor(value):
+    return math.floor(float(value))
+
+@register.filter
+def get_decimal(value):
+    return float(value) % 1
+
+@register.filter
+def to_feet(value):
+    return str(floor(float(value))) + " ft " + str(round(get_decimal(value) * 12.0)) + " in"
