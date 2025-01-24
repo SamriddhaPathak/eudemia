a60105b3737330354e15d3c9de52cd21f51e2b32
Rasum <rasum.subedi13@gmail.com>
2025-01-24 21:05:30 +0545

    fix: make www.rasum.com.np accessible

--- eudemia/settings/production.py
+++ eudemia/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["rasum.com.np", "media.rasum.com.np"]
+ALLOWED_HOSTS = ["rasum.com.np", "media.rasum.com.np", "www.rasum.com.np"]
 
 MEDIA_URL = "https://media.rasum.com.np/"
 MEDIA_ROOT = "/home2/rasumcom/media.rasum.com.np/"
